a237b665b11139d60377eb7cf3567133b4697076
jookllo <hokello34@gmail.com>
2020-08-15 00:48:46 +0300

    Added a sentence to allow for editing. Bug is currently there.

--- LoonyLips.gd
+++ LoonyLips.gd
@@ -4,9 +4,7 @@ var person = ["Kijana", "Mjinga", "Nyumbani"]
 
 func _ready():
 	print("My name is " + person[1]+ " and I hope maisha iko "+ person[2])
-
 	$VBoxContainer/Words.text = "Hii NI text BudAAAAAAA " + person[2]
-	
 
 func _on_PlayerText_text_entered(new_text):
 	updatePlayerText(new_text)

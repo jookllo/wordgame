badbbab636643707958eaa14788c630c52ef1151
jookllo <hokello34@gmail.com>
2020-08-15 00:49:44 +0300

    Updated file

--- LoonyLips.gd
+++ LoonyLips.gd
@@ -1,18 +1,40 @@
 extends Control
 
-var person = ["Kijana", "Mjinga", "Nyumbani"]
+var player_words = []
+var prompts = ["a name", "a noun", "adverb","adjective"]
+var welcome = "Welcome to Loony Lips. Add words as instructed."
+var story = "Once upon a time, %s decided to go to %s for a meal. During the meal he ate %s and thus got a stomach ache and took an %s lemon for stomach relief."
+onready var Playertext = $VBoxContainer/HBoxContainer/PlayerText
+onready var Words = $VBoxContainer/Words
 
 func _ready():
-	print("My name is " + person[1]+ " and I hope maisha iko "+ person[2])
-	$VBoxContainer/Words.text = "Hii NI text BudAAAAAAA " + person[2]
-
+	Words.text = welcome
+	check_player_words_length()
+	
 func _on_PlayerText_text_entered(new_text):
-	updatePlayerText(new_text)
+	add_to_player_words()
 
 func _on_TextureButton_pressed():
-	var words = $VBoxContainer/HBoxContainer/PlayerText.text
-	updatePlayerText(words)
+	add_to_player_words()
 
-func updatePlayerText (new_text):
-	$VBoxContainer/Words.text = new_text
-	$VBoxContainer/HBoxContainer/PlayerText.clear()
+
+func add_to_player_words():
+	player_words.append(Playertext.text)
+	Words.text = " "
+	Playertext.clear()
+	check_player_words_length()
+
+func is_story_done():
+	return player_words.size() == prompts.size()
+
+func check_player_words_length():
+	if is_story_done():
+		tell_story()
+	else:
+		prompt_player()
+
+func tell_story():
+	Words.text = story % player_words
+
+func prompt_player():
+	Words.text += "May I have " + prompts[player_words.size()] + " please?"
